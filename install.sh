1d6068428ff66ccae66c7a8675d54146639a78ca
MarkRaub <markraub@yahoo.com>
2016-11-04 15:14:25 +0000

    added Firmware folder for scanner firmware, and an installer script for the required repos, it also installs the firmware

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install sane sane-utils libsane:armhf libsane-common libsane-dev libsane-extras-common libsane-extras-dev
+
+dpkg -i Firmware/libsane-epson-perfection-1670.deb

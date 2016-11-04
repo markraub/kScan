0ea943a32b63a6904cb09659426d6c8b10102fd7
MarkRaub <markraub@yahoo.com>
2016-11-04 13:28:48 +0000

    added phyisical ibutton reader support, currently running from test_ibutton

--- kScan.py
+++ kScan.py
@@ -3,12 +3,49 @@ import os
 import json
 import sane
 from PIL import Image
+import get_user as guh
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+def get_ibutton():
+
+        os.system('modprobe wire timeout=1 slave_ttl=5')
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-smem')
+
+        GPIO.setup(24,GPIO.OUT)
+        base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
+        delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
+        GPIO.output(24,True)
+
+        while True:
+
+                data = open(base_dir, "r")
+                ibutton = data.read()
+                data.close()
+                if ibutton != 'not found.\n':
+                        GPIO.output(24,False)
+                        time.sleep(3)
+                        print(ibutton)
+                        tester(ibutton)
+                        d = open(delete_dir, "w")
+                        d.write(ibutton)
+                        d.close()
+                        GPIO.output(24, True)
+
+        GPIO.cleanup()
+
+
+
 
 
 def transferFile(user, homedir, imageName):
 
 	if user is not None and homedir is not None and image is not None:
 		
+		image = takeScan()
 		imagepath = "/scans/" + imageName
 		status = os.system('scp ' + imagepath + user + "@shell.csh.rit.edu:" + homedir + ".scan/"
 		print(status)
@@ -30,3 +67,7 @@ def takeScan():
 	im = sel_dev.snap()
 
 	im.save('test_pil.jpeg')
+	
+	return im
+
+
--- test_ibutton.py
+++ test_ibutton.py
@@ -1,8 +1,49 @@
 import get_user as guh
+import os
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
 
-def main():
+def tester(varID):
+	
+	print(guh.read_ibutton(varID))
 
-	print(guh.read_ibutton("67000001ECFCC601"))
+def get_ibutton():
 
-main()
+	os.system('modprobe wire timeout=1 slave_ttl=5')
+	os.system('modprobe w1-gpio')
+	os.system('modprobe w1-smem')
+
+	GPIO.setup(24,GPIO.OUT)
+	base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
+	delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
+	GPIO.output(24,True)
+	
+	while True:
+	
+		data = open(base_dir, "r")
+		ibutton = data.read()
+		data.close()
+		if ibutton != 'not found.\n':
+			GPIO.output(24,False)
+			time.sleep(3)
+			print(ibutton)
+			tester(ibutton)
+			d = open(delete_dir, "w")
+			d.write(ibutton)
+			d.close()
+			GPIO.output(24, True)
+
+	GPIO.cleanup()
+	
+get_ibutton()
+
+
+
+
+
+
+
+
 

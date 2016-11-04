6f41fc0afda9bce4109d0811c3a987402f3a208b
Mark Raub <markraub@Marks-MacBook-Pro.local>
2016-11-04 09:49:22 -0400

    combined everything into kScan.py

--- kScan.py
+++ kScan.py
@@ -1,19 +1,60 @@
-import ibutton 
 import os
-import json
 import sane
-from PIL import Image
-import get_user as guh
 import time
 import RPi.GPIO as GPIO
+import credentials as login
+import ldap
 GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
 
-def get_ibutton():
+try:
+    GPIO.cleanup()
+except:
+    pass
 
+
+def main():
+
+    print("Lets set up that iButton Reader")
+    time.sleep(1)
+    try:
         os.system('modprobe wire timeout=1 slave_ttl=5')
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-smem')
+    except:
+
+        print("You probably need to re-run this as sudo buddy...this is kinda embarrassing...\n")
+        return
+
+    print("We did it!\nGo ahead and start scanning my dude!")
+    time.sleep(2)
+    get_ibutton()
+
+
+def find_user(varID, cache={}):
+
+    ibutton = varID.strip()
+
+    if ibutton in cache:
+        return cache[ibutton]
+    try:
+
+        #conn = ldap.initialize(login.ldap_server, bytes_mode=True)
+        conn = ldap.initialize(login.ldap_server)
+
+        conn.simple_bind_s(login.ldap_user, login.ldap_pass)
+        ldap_results = conn.search_s('ou=Users,dc=csh,dc=rit,dc=edu', ldap.SCOPE_SUBTREE, "(ibutton=%s)" % ibutton,
+                ['uid', 'homeDirectory'])
+        print('(ibutton=%s)' % ibutton)
+
+        return ldap_results[0][1]['uid'][0]
+    except Exception as e:
+        print(e)
+        print("\n")
+        print("were having some trouble getting your user ID, try scanning again")
+        return None
+
+
+def get_ibutton():
 
         GPIO.setup(24,GPIO.OUT)
         base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
@@ -29,45 +70,63 @@ def get_ibutton():
                         GPIO.output(24,False)
                         time.sleep(3)
                         print(ibutton)
-                        tester(ibutton)
+
+                        try:
+
+                            user = find_user()
+                            if user is None:
+                                print("Cannot scan, ldap didn't give me a user")
+
+                            else:
+
+                                print(user)
+                                transferFile(user)
+
+                        except Exception as e:
+
+                            print(e)
+                            print("Captain, I'm afraid the ship is sinking")
+
+
+
+                        
                         d = open(delete_dir, "w")
                         d.write(ibutton)
-                        d.close()
                         GPIO.output(24, True)
 
+        d.close()
         GPIO.cleanup()
 
 
+def transferFile(user):
+
+        if user is not None:
+
+            takeScan(user)
+            imagepath = "/scans/" + user + ".jpeg"
+            os.system('scp ' + imagepath + " " + user + "@shell.csh.rit.edu:~/.scan/")
 
 
+def takeScan(user):
 
-def transferFile(user, homedir, imageName):
+        ver = sane.init()
+        print("SANE Version: " + ver)
 
-	if user is not None and homedir is not None and image is not None:
-		
-		image = takeScan()
-		imagepath = "/scans/" + imageName
-		status = os.system('scp ' + imagepath + user + "@shell.csh.rit.edu:" + homedir + ".scan/"
-		print(status)
+        devices = sane.get_devices()
 
-def takeScan():
+        sel_dev = sane.open(devices[0][0])
 
-	ver = sane.init()
-	print("SANE Version: " + ver)
-	
-	devices = sane.get_devices()
-	
-	sel_dev = sane.open(devices[0][0])
+        sel_dev.start()
 
-	sel_dev.start()
+        print("Starting device")
+        print("Scanning...")
 
-	print("Starting device")
-	print("Scanning...")
+        im = sel_dev.snap()
 
-	im = sel_dev.snap()
+        im.save()
 
-	im.save('test_pil.jpeg')
-	
-	return im
+        return im
 
+if __name__ == "__main__":
 
+        main()
